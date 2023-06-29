0622650d7ccdd05cf75a3a63f27a33f095114b44
Alexander-Yershov <alex04398956@gmail.com>
2023-06-29 13:15:46 +0300

    siblings
    добавляет методы для работы с соседними элементами

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,9 @@ $('div').click(function () {
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.some'));
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme').addClass('dfhsjfk'));
+console.log($('.findme').siblings());
 // console.log($('button').html('hello'));
 
 
